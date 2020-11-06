1cfb973b5721a9ba6e4be2e2ae973b74dfea6bdb
Doan Cong Thanh <thanhdoan020999@gmail.com>
2020-11-06 21:08:03 +0700

    sign in + sign up

--- controller/users.js
+++ controller/users.js
@@ -5,20 +5,43 @@ const user = require('../models/users')
 const userController = {};
 
 userController.getUser = async (req, res, next) => {
-    const usersList = await user.find({ username: 'admin' });
+    const usersList = await user.find();
     console.log(usersList);
     res.send(JSON.stringify(usersList));
 }
 
-userController.loginUser = async (req, res, next) => {
-    const userData = req.body;
+userController.signUp = async (req, res, next) => {
+    const userData = req.body
     const newUser = new user({
         username: userData.username,
         password: userData.password,
+        email: userData.email,
+        firstName: userData.firstname,
+        lastName: userData.lastname
     });
     try {
-        newUser.save();
-        res.send("OK");
+        const user = await newUser.save();
+        console.log(user)
+        res.send(user);
+        console
+    } catch (err) {
+        if (err)
+            console.log(err);
+    }
+}
+
+userController.signIn = async (req, res, next) => {
+
+    try {
+        const userdata = await user.find({ username: req.body.username })
+        if (userdata.length == 0) {
+            res.send("Account not exsit")
+        }
+        else {
+            if (req.body.password == userdata.password) {
+                res.redirect('/user')
+            }
+        }
     } catch (err) {
         if (err)
             console.log(err);
